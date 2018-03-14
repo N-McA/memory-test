1e7337430b266fccf20207632c6c347976bb5c01
Nat McAleese <git@nat-mcaleese.co.uk>
2018-03-14 13:53:59 +0000

    appears to work on the sandbox...

--- src/Completed.js
+++ src/Completed.js
@@ -2,50 +2,22 @@
 import React, { Component } from 'react';
 import { getURLParams } from './utils.js';
 
-function amazonSubmit(obj) {
-  let params = getURLParams()
-  let url = params.turkSubmitTo 
-  url = url + '/mturk/externalSubmit'
-  url = url + '?assignmentId=' + params.assignmentId
-  url = url + "&data=done";
-  return fetch(url, {
-    method: 'get',
-  })
-}
-
-function sendData(logObj) {
-  return amazonSubmit(logObj)
-}
 
 class Completed extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      ready: false
-    }
-    sendData()
-      .then(() => this.setState({ready: true}))
+    this.urlParams = getURLParams()
   }
   render() {
     return (
       <div className="Completed">
-        {
-          this.state.ready &&
-          (<div>
-          <h1>Experiment Completed!</h1>
-          <p>
-            Thank you for your participation!
-          </p>
-
-          <p>
-            Your results will contribute to science.
-          </p>
-
-          <p>
-            Please contact Nat McAleese (nm583) if you have any questions.
-          </p>
-          </div>)
-        }
+        <form 
+          id="turkSubmit" 
+          action={this.urlParams.turkSubmitTo + "/mturk/externalSubmit"} 
+          method="post">
+            <input type="hidden" name="assignmentId" id="assignmentId" value={this.urlParams.assignmentId}/>
+            <input type="submit" value="Click to Complete Experiment" name="submitButton" id="completeButton" />
+        </form>
       </div>
     );
   }
